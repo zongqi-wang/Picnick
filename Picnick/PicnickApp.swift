23e814e155dbc72a8c619b573c35544a6a4a59d7
Cedric Wang <wangzongqi@msn.com>
2023-02-26 22:40:58 -0800

    Initial Commit

---
+++ Picnick/PicnickApp.swift
@@ -0,0 +1,17 @@
+//
+//  PicnickApp.swift
+//  Picnick
+//
+//  Created by Zongqi Wang on 2/26/23.
+//
+
+import SwiftUI
+
+@main
+struct PicnickApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
